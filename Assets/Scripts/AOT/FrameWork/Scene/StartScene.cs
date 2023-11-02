1cdb34f4e05155811c9437c914304bd1e4f3309e
Charlotte <a651061950@gmail.com>
2023-11-02 22:33:52 +0800

    提交上传2023年11月2日22:33:48

--- Assets/Scripts/AOT/FrameWork/Scene/StartScene.cs
+++ Assets/Scripts/AOT/FrameWork/Scene/StartScene.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class StartScene : BaseScene
 {
     [SerializeField] private UIButton Btn_GameStart;
+    [SerializeField] private UIButton GuideTestBtn;
+    [SerializeField] private RectTransform Trans_Canvas;
+    [SerializeField] private RectTransform Trans_FocusMask;
     public override async UniTask InitializeAsync(object param = null)
     {
         await base.InitializeAsync(param);
@@ -14,5 +18,24 @@ public class StartScene : BaseScene
         {
             Log.Debug("Start Game");
         });
+        
+        GuideTestBtn.onClick.SubscribeAsync(async () =>
+        {
+            Log.Debug("触发引导");
+        });
+        SetFocusPosition(GuideTestBtn.GetComponent<RectTransform>());
+    }
+
+    private void SetFocusPosition(RectTransform buttonRect)
+    {
+        Vector2 size = Vector2.Scale(buttonRect.rect.size, transform.lossyScale);
+        var rect = new Rect((Vector2)buttonRect.position-(size * buttonRect.pivot), size);
+        var center = rect.center;
+        var uicamera = Camera.main;
+        Vector2 centerScreen = uicamera.WorldToScreenPoint(center);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(Trans_Canvas, centerScreen, uicamera, out Vector2 t);
+        Trans_FocusMask.anchoredPosition = t;
+        Trans_FocusMask.sizeDelta = buttonRect.rect.size * buttonRect.localScale;
+
     }
 }
