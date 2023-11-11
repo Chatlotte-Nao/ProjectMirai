455173467d87128c6f4b0660cb541f2cc4a3af41
Charlotte <a651061950@gmail.com>
2023-11-11 22:39:19 +0800

    提交上传2023年11月11日22:39:15

---
+++ Assets/Scripts/AOT/FrameWork/Util/ResourceManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+/// <summary>
+/// 资源管理器、进行各种类型资源的加载、释放
+/// </summary>
+public class ResourceManager : Singleton<ResourceManager>
+{
+    
+
+
+
+
+    public async UniTask<GameObject> LoadPrefabAsync(string name,bool isAot=false)
+    {
+#if UNITY_EDITOR
+        GameObject go= AssetDatabase.LoadAssetAtPath<GameObject>(name);
+        return go;
+#endif
+    }
+
+    public GameObject LoadPrefab(string name,bool isAot=false)
+    {
+        return null;
+    }
+}
